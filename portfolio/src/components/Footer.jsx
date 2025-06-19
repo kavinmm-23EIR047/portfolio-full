43113bf251d6a1a1017eaa6a31a271581bd83810
kavin <kavinmm.23eie@kongu.edu>
2025-06-19 19:51:57 +0530

    1

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,11 +1,11 @@
-
 const Footer = () => {
-    return (
-      <footer className="bg-gray-900 text-white text-center py-6">
-        <p className="text-gray-400">© 2025 Kavin's Portfolio. All Rights Reserved.</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer className="bg-gray-900 text-white text-center py-6">
+      <p className="text-gray-300 font-semibold">© 2025 Kavin's Portfolio | AK WebFlair Technologies</p>
+      <p className="text-gray-400">📧 Email: <a href="kavinmm200@gmail.com" className="underline">kavinmm200@gmail.com</a></p>
+      <p className="text-gray-400">📞 Contact: +91 9600732162</p>
+    </footer>
+  );
+};
+
+export default Footer;
